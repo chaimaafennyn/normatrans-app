9787e5dd64986b6fd8271c50d32f49d1d374c561
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-04-22 10:13:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("Normatrans - Analyse des zones de livraison")
 
 # Chargement des données
 try:
-    df = pd.read_csv("/content/drive/MyDrive/projet_normatrans_zone/nettoyage_de_donnee/zones_final_localites.csv", sep=";", encoding="utf-8")
+    df = pd.read_csv("zones_final_localites.csv", sep=";", encoding="utf-8")
     df.columns = df.columns.str.strip()
     st.success("Données chargées avec succès")
 except Exception as e:
