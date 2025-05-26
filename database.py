a06e1b4847f01a11b2ae1825c2582cba647acebf
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-05-26 13:56:20 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,30 +1,18 @@
 # database.py
-
 from sqlalchemy import create_engine
 import pandas as pd
-import streamlit as st
 
-# Connexion à Supabase
+DB_USER = "postgres.rxtcigbzpbppqianbdcn"
+DB_PASSWORD = "chaimaafennyn"
+DB_HOST = "aws-0-eu-west-3.pooler.supabase.com"
+DB_PORT = "6543"
+DB_NAME = "postgres"
+
 def get_engine():
-    db = st.secrets["database"]
-    url = f"postgresql://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['dbname']}"
+    url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     return create_engine(url)
 
-# Lecture des tables
 def get_zones():
     engine = get_engine()
-    return pd.read_sql("SELECT * FROM zones_localites", engine)
-
-def get_tranches():
-    engine = get_engine()
-    return pd.read_sql("SELECT * FROM tranche_zone", engine)
-
-# Exemple de fonction pour ajouter une ligne
-def insert_tranche(tranche, zone, valeur):
-    engine = get_engine()
-    query = f"""
-        INSERT INTO tranche_zone (tranche, zone, valeur)
-        VALUES ('{tranche}', '{zone}', {valeur});
-    """
-    with engine.connect() as conn:
-        conn.execute(query)
+    query = "SELECT * FROM zones_localites"
+    return pd.read_sql(query, engine)
