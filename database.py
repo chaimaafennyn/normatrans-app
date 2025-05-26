10442d7ba894e3daa63433b7560881be6bb68d3f
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-05-26 13:41:12 +0200

    Update database.py

--- database.py
+++ database.py
@@ -15,32 +15,3 @@ def get_tranches():
     engine = get_engine()
     return pd.read_sql("SELECT * FROM tranche_zone", engine)
 
-def log_action(username, action):
-    engine = get_engine()
-    query = f"""
-        INSERT INTO logs (username, action)
-        VALUES (%s, %s);
-    """
-    with engine.connect() as conn:
-        conn.execute(query, (username, action))
-
-
-def insert_localite(commune, code_agence, zone, lat, lon, distance, lat_ag, lon_ag):
-    engine = get_engine()
-    with engine.begin() as conn:
-        conn.execute(
-            text("""
-                INSERT INTO zones_localites (commune, code_agence, zone, latitude, longitude, distance_km, latitude_agence, longitude_agence)
-                VALUES (:commune, :code_agence, :zone, :lat, :lon, :distance, :lat_ag, :lon_ag)
-            """),
-            {
-                "commune": commune,
-                "code_agence": code_agence,
-                "zone": zone,
-                "lat": lat,
-                "lon": lon,
-                "distance": distance,
-                "lat_ag": lat_ag,
-                "lon_ag": lon_ag,
-            }
-        )
