08287c568f75c80e12b4c1ce9210be86131de547
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-05-26 13:01:29 +0200

    Update 3_Calcul_Tarifs.py

--- pages/3_Calcul_Tarifs.py
+++ pages/3_Calcul_Tarifs.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
+    st.warning("🚫 Accès non autorisé. Veuillez vous connecter depuis la page principale.")
+    st.stop()
+    
 st.title("💶 Calcul des Tarifs par Tranche")
 
 # === Répartition (en %) des zones par tranche
