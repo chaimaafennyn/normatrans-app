165f42bc8bf791c0aab1c86e8ec6aa1a034a72ba
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-05-26 13:57:51 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,11 @@
-# database.py
+import streamlit as st
 from sqlalchemy import create_engine
 import pandas as pd
 
-DB_USER = "postgres.rxtcigbzpbppqianbdcn"
-DB_PASSWORD = "chaimaafennyn"
-DB_HOST = "aws-0-eu-west-3.pooler.supabase.com"
-DB_PORT = "6543"
-DB_NAME = "postgres"
+db = st.secrets["database"]
 
 def get_engine():
-    url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    url = f"postgresql://{db.user}:{db.password}@{db.host}:{db.port}/{db.dbname}"
     return create_engine(url)
 
 def get_zones():
