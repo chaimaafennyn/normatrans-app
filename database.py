66371f140b1b844ed5a5fa0e8acb9f103bdf88d9
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-05-26 14:44:51 +0200

    Update database.py

--- database.py
+++ database.py
@@ -18,44 +18,56 @@ def get_tranches():
     query = "SELECT * FROM tranche_zone"
     return pd.read_sql(query, engine)
 
-def insert_localite(data: dict):
-    engine = get_engine()
-    with engine.connect() as conn:
-        insert_query = text("""
-            INSERT INTO zones_localites (
-                commune, code_agence, latitude, longitude, zone,
-                distance_km, latitude_agence, longitude_agence
-            ) VALUES (
-                :commune, :code_agence, :latitude, :longitude, :zone,
-                :distance_km, :latitude_agence, :longitude_agence
-            )
-        """)
-        conn.execute(insert_query, data)
-        conn.commit()
 
-def update_localite(id, data: dict):
-    engine = get_engine()
-    with engine.connect() as conn:
-        update_query = text("""
-            UPDATE zones_localites SET
-                commune = :commune,
-                code_agence = :code_agence,
-                latitude = :latitude,
-                longitude = :longitude,
-                zone = :zone,
-                distance_km = :distance_km,
-                latitude_agence = :latitude_agence,
-                longitude_agence = :longitude_agence
-            WHERE id = :id
-        """)
-        data["id"] = id
-        conn.execute(update_query, data)
-        conn.commit()
 
-def delete_localite(id):
+def insert_zone(commune, zone, code_agence, lat, lon, lat_ag, lon_ag, distance):
     engine = get_engine()
-    with engine.connect() as conn:
-        delete_query = text("DELETE FROM zones_localites WHERE id = :id")
-        conn.execute(delete_query, {"id": id})
-        conn.commit()
+    query = text("""
+        INSERT INTO zones_localites (commune, zone, code_agence, latitude, longitude, latitude_agence, longitude_agence, distance_km)
+        VALUES (:commune, :zone, :code_agence, :lat, :lon, :lat_ag, :lon_ag, :distance)
+    """)
+    with engine.begin() as conn:
+        conn.execute(query, {
+            "commune": commune,
+            "zone": zone,
+            "code_agence": code_agence,
+            "lat": lat,
+            "lon": lon,
+            "lat_ag": lat_ag,
+            "lon_ag": lon_ag,
+            "distance": distance
+        })
+
+def update_zone(id, commune, zone, code_agence, lat, lon, lat_ag, lon_ag, distance):
+    engine = get_engine()
+    query = text("""
+        UPDATE zones_localites
+        SET commune = :commune,
+            zone = :zone,
+            code_agence = :code_agence,
+            latitude = :lat,
+            longitude = :lon,
+            latitude_agence = :lat_ag,
+            longitude_agence = :lon_ag,
+            distance_km = :distance
+        WHERE id = :id
+    """)
+    with engine.begin() as conn:
+        conn.execute(query, {
+            "id": id,
+            "commune": commune,
+            "zone": zone,
+            "code_agence": code_agence,
+            "lat": lat,
+            "lon": lon,
+            "lat_ag": lat_ag,
+            "lon_ag": lon_ag,
+            "distance": distance
+        })
+
+def delete_zone(id):
+    engine = get_engine()
+    query = text("DELETE FROM zones_localites WHERE id = :id")
+    with engine.begin() as conn:
+        conn.execute(query, {"id": id})
 
