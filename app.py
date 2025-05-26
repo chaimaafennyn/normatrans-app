d7aca5faf077cdf42b6130ef956e774cb5e973ec
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-05-26 12:50:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from auth import check_password, logout
 
-st.set_page_config(page_title="Normatrans", layout="wide")
-
 # Sécurité
 check_password()
 logout()
+
+st.set_page_config(page_title="Normatrans", layout="wide")
 
 st.title("🚚 Normatrans - Zones et Tarifs")
 st.info("👈 Utilisez le menu à gauche pour naviguer entre les pages.")
