387450eb25fd90547d724bee16c0161bcbb00862
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-05-28 09:40:58 +0200

    Update 6_Analyse_Pal.py

--- pages/6_Analyse_Pal.py
+++ pages/6_Analyse_Pal.py
@@ -17,3 +17,10 @@ df["UM"] = df["UM"].astype(str).str.replace(",", ".").astype(float)
 df["Zone"] = df["Zone"].str.strip()
 if "Code agence" in df.columns:
     df["Code agence"] = df["Code agence"].astype(str).str.strip()
+
+bins_um = [0, 0.5, 1, 2, 3, 4, 5, 10, float("inf")]
+labels_um = ["0-0.5", "0.5-1", "1-2", "2-3", "3-4", "4-5", "5-10", ">10"]
+
+df["Tranche UM"] = pd.cut(df["UM"], bins=bins_um, labels=labels_um, right=False)
+df = df[df["Tranche UM"].notna()]
+
