6c8f3f0c21c54d9749fb9ee79a68cd464d1a1fdd
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-05-28 09:30:13 +0200

    Update 6_Analyse_Pal.py

--- pages/6_Analyse_Pal.py
+++ pages/6_Analyse_Pal.py
@@ -23,24 +23,6 @@ labels = ["1 palette", "2 palettes", "3 palettes", "4 palettes", "5 palettes", "
 df["Tranche_UM"] = pd.cut(df["UM"], bins=bins, labels=labels, right=True)
 df = df[df["Tranche_UM"].notna()]
 
-# Répartition par zone
-st.subheader("📊 Répartition (%) des tranches de palettes par zone")
-pivot = df.groupby(["Zone", "Tranche_UM"]).size().reset_index(name="Nb_exp")
-totaux = pivot.groupby("Zone")["Nb_exp"].sum().reset_index(name="Total")
-result = pd.merge(pivot, totaux, on="Zone")
-result["Pourcentage"] = (result["Nb_exp"] / result["Total"] * 100).round(2)
-tableau = result.pivot(index="Zone", columns="Tranche_UM", values="Pourcentage").fillna(0)
-st.dataframe(tableau)
-
-
-# Graphique global
-st.subheader("📈 Répartition globale des tranches de palette")
-global_counts = df["Tranche_UM"].value_counts(normalize=True).sort_index() * 100
-fig = px.bar(global_counts, x=global_counts.index, y=global_counts.values,
-             labels={'x': "Tranche de palette", 'y': "Pourcentage"},
-             title="Distribution globale des tranches de palettes")
-st.plotly_chart(fig)
-
 # === Zones par tranches UM
 st.subheader("🔁 Répartition (%) des zones par tranche de palette (UM)")
 
@@ -60,4 +42,24 @@ st.download_button(
     file_name="zones_par_tranche_um.csv",
     mime="text/csv"
 )
+
+# Répartition par zone
+st.subheader("📊 Répartition (%) des tranches de palettes par zone")
+pivot = df.groupby(["Zone", "Tranche_UM"]).size().reset_index(name="Nb_exp")
+totaux = pivot.groupby("Zone")["Nb_exp"].sum().reset_index(name="Total")
+result = pd.merge(pivot, totaux, on="Zone")
+result["Pourcentage"] = (result["Nb_exp"] / result["Total"] * 100).round(2)
+tableau = result.pivot(index="Zone", columns="Tranche_UM", values="Pourcentage").fillna(0)
+st.dataframe(tableau)
+
+
+# Graphique global
+st.subheader("📈 Répartition globale des tranches de palette")
+global_counts = df["Tranche_UM"].value_counts(normalize=True).sort_index() * 100
+fig = px.bar(global_counts, x=global_counts.index, y=global_counts.values,
+             labels={'x': "Tranche de palette", 'y': "Pourcentage"},
+             title="Distribution globale des tranches de palettes")
+st.plotly_chart(fig)
+
+
 
