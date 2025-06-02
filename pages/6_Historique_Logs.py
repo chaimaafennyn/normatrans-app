153cff148a7353cca0c4a75d33d40f7a7473d108
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-06-02 09:15:49 +0200

    Update 6_Historique_Logs.py

--- pages/6_Historique_Logs.py
+++ pages/6_Historique_Logs.py
@@ -1,10 +1,14 @@
 import streamlit as st
-import pandas as pd
-from sqlalchemy import text
 from database import get_engine
+import pandas as pd
 
-st.set_page_config(page_title="🛠️ Gestion des Localités", layout="wide")
-
+# === Authentification
 if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
     st.warning("🚫 Accès non autorisé. Veuillez vous connecter depuis la page principale.")
     st.stop()
+
+st.title("🕵️ Historique des actions")
+
+engine = get_engine()
+df_logs = pd.read_sql("SELECT * FROM logs ORDER BY timestamp DESC", engine)
+st.dataframe(df_logs)
