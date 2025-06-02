a751f768964656016d91e284552aceb734fe8149
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-06-02 09:15:39 +0200

    Update 5_Calcul_tarifs_palette.py

--- pages/5_Calcul_tarifs_palette.py
+++ pages/5_Calcul_tarifs_palette.py
@@ -1,20 +1,4 @@
 import streamlit as st
-from database import get_engine
-import pandas as pd
-
-# === Authentification
-if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
-    st.warning("🚫 Accès non autorisé. Veuillez vous connecter depuis la page principale.")
-    st.stop()
-
-st.title("🕵️ Historique des actions")
-
-engine = get_engine()
-df_logs = pd.read_sql("SELECT * FROM logs ORDER BY timestamp DESC", engine)
-st.dataframe(df_logs)
-
-
-import streamlit as st
 import pandas as pd
 import math
 
