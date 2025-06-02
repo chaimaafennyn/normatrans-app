dbc7fa6c3b208d81615f1854ca88903980622249
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-06-02 09:13:45 +0200

    Update 6_Historique_Logs.py

--- pages/6_Historique_Logs.py
+++ pages/6_Historique_Logs.py
@@ -1,1 +1,4 @@
-
+# === Authentification
+if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
+    st.warning("🚫 Accès non autorisé. Veuillez vous connecter depuis la page principale.")
+    st.stop()
