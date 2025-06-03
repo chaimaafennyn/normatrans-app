e9393c38ee3de39b02ad3635a8f3805fde72de0f
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-06-03 09:17:47 +0200

    Update 6_Historique_Logs.py

--- pages/6_Historique_Logs.py
+++ pages/6_Historique_Logs.py
@@ -1,17 +1,20 @@
 import streamlit as st
-from database import get_engine, log_action
 import pandas as pd
+from database import get_engine
 
 # === Authentification
 if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
     st.warning("🚫 Accès non autorisé. Veuillez vous connecter depuis la page principale.")
     st.stop()
 
-if role == "admin":
-    st.title("🕵️ Historique des actions")
-    engine = get_engine()
-    df_logs = pd.read_sql("SELECT * FROM logs ORDER BY timestamp DESC", get_engine())
-    st.dataframe(df_logs)
-    
-else:
-    st.info("🔒 Lecture seule : vous n'avez pas les droits pour modifier les données.")
+# === Vérification du rôle
+if st.session_state.get("role") != "admin":
+    st.error("🔒 Accès réservé à l'administrateur.")
+    st.stop()
+
+st.title("🕵️ Historique des actions")
+
+engine = get_engine()
+df_logs = pd.read_sql("SELECT * FROM logs ORDER BY timestamp DESC", engine)
+
+st.dataframe(df_logs)
