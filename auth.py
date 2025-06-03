77d95ea8feb9e9c3cab045d85dc541ada3199abd
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-06-03 10:30:16 +0200

    Update auth.py

--- auth.py
+++ auth.py
@@ -30,6 +30,15 @@ def check_password():
                 st.rerun()
             else:
                 st.error("❌ Identifiants incorrects.")
+
+            if username in CREDENTIALS and CREDENTIALS[username] == password:
+                st.session_state["authenticated"] = True
+                st.session_state["username"] = username
+            
+                log_action(username, "Connexion", "Utilisateur connecté à l'application")  
+            
+                st.success("✅ Connexion réussie.")
+                st.rerun()
         st.stop()
 
 def logout():
