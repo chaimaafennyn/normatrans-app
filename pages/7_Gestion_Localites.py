c4e96c447f81a1a776c39860be8159c913259531
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-06-04 16:10:51 +0200

    Update 7_Gestion_Localites.py

--- pages/7_Gestion_Localites.py
+++ pages/7_Gestion_Localites.py
@@ -3,11 +3,12 @@ import pandas as pd
 from sqlalchemy import text
 from database import get_engine
 
-st.set_page_config(page_title="🛠️ Gestion des Localités", layout="wide")
 
 if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
     st.warning("🚫 Accès non autorisé. Veuillez vous connecter depuis la page principale.")
     st.stop()
+
+st.set_page_config(page_title="🛠️ Gestion des Localités", layout="wide")
 
 st.title("🛠️ Gestion des Localités depuis Supabase")
 engine = get_engine()
