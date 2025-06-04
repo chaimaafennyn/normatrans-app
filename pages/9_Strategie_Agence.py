afd8c60fe84d01f7c5b0d5c664d16d1b46ed3d34
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-06-04 11:43:39 +0200

    Update 9_Strategie_Agence.py

--- pages/9_Strategie_Agence.py
+++ pages/9_Strategie_Agence.py
@@ -104,7 +104,7 @@ if len(df_eloignees) > 0:
 st.subheader("🏗️ Suggestion intelligente d’ouverture d’agence")
 
 seuil_distance = 40
-seuil_nb_exp = 2  
+seuil_nb_exp = 4  
 
 clusters_concernes = df_unique[
     (df_unique["Distance (km)"] > seuil_distance) & (df_unique["Nb_expéditions"] > seuil_nb_exp)
